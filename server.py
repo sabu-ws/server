cd3645886baad1a72d971f61bcb0920cf6152969
root <pubtranos@gmail.com>
2023-11-07 06:39:08 -0500

    try

--- server.py
+++ server.py
@@ -4,8 +4,9 @@ import eventlet
 
 if __name__ == '__main__':
 
-	eventlet.monkey_patch()
+	# eventlet.monkey_patch()
 	cert_file="/sabu/ssl/sabu.crt"
-	key_file="/sabu/ssl/private/saby.key"
+	key_file="/sabu/ssl/private/sabu.key"
 
 	socketio.run(app,"127.0.0.1", 8888, certfile=cert_file, keyfile=key_file, debug=True)
+	# socketio.run(app,"0.0.0.0", 8888, debug=True)
