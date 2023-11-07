c0fbd81f614db21067c34e71a5af335b60a6e24c
Minvinea <sabu-ws@pronton.me>
2023-11-07 11:38:38 +0100

    https

--- server.py
+++ server.py
@@ -3,5 +3,9 @@ from app import app
 import eventlet
 
 if __name__ == '__main__':
-	# eventlet.monkey_patch()
-	socketio.run(app,"0.0.0.0", 8888, debug=True)
+
+	eventlet.monkey_patch()
+	cert_file="/sabu/ssl/sabu.crt"
+	key_file="/sabu/ssl/private/saby.key"
+
+	socketio.run(app,"127.0.0.1", 8888, certfile=cert_file, keyfile=key_file, debug=True)
