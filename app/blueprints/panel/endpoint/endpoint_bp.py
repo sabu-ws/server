b7112e296fbb10df7e3ae2fc32ca5badfb7a87d9
LooXzy <looxzy@protonmail.com>
2023-11-11 17:43:37 +0100

    add endpoints pages + error 404 + edit subnavbar

--- app/blueprints/panel/endpoint/endpoint_bp.py
+++ app/blueprints/panel/endpoint/endpoint_bp.py
@@ -8,3 +8,18 @@ endpoint_bp = Blueprint("endpoint", __name__)
 @endpoint_bp.route("/")
 def index():
     return render_template("ap_ep_index.html")
+
+
+@endpoint_bp.route("/dashboard")
+def dashboard():
+	return render_template("ap_ep_dashboard.html")
+
+
+# @endpoint_bp.route("/logs")
+# def logs():
+# 	return render_template("ap_ep_logs.html")
+
+
+@endpoint_bp.route("/settings")
+def settings():
+	return render_template("ap_ep_settings.html")
