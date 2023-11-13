6e3877572db68b8034bf07e13cf6f9b887a62c98
tranosgames <pubtranos@gmail.com>
2023-11-13 13:36:13 -0500

    modify db

--- app/models.py
+++ app/models.py
@@ -5,50 +5,57 @@ from sqlalchemy import DateTime
 
 
 class Users(db.Model, UserMixin):
-    __tablename__ = "Users"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    uuid = db.Column(db.String(255), unique=True)
-    name = db.Column(db.String(255), nullable=True)
-    firstname = db.Column(db.String(255), nullable=True)
-    username = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=True)
-    job = db.Column(db.Integer, db.ForeignKey("Job.id"))
-    role = db.Column(db.String(64), nullable=False)
-    cookie = db.Column(db.String(255), unique=True, nullable=True)
-    OTPSecret = db.Column(db.String(255), unique=True, nullable=True)
-    codeEP = db.Column(db.String(255), unique=True, nullable=True)
-    picture = db.Column(db.String(1024), unique=True, nullable=True)
-    enable = db.Column(db.Integer, nullable=True, default=1)
-    firstCon = db.Column(db.Integer, default=0)
+	__tablename__ = "Users"
+	id = db.Column(db.Integer, primary_key=True, unique=True)
+	uuid = db.Column(db.String(255), unique=True)
+	name = db.Column(db.String(255), nullable=True)
+	firstname = db.Column(db.String(255), nullable=True)
+	username = db.Column(db.String(255), nullable=False)
+	password = db.Column(db.String(255), nullable=False)
+	email = db.Column(db.String(255), nullable=True)
+	job = db.Column(db.Integer, db.ForeignKey("Job.id"))
+	role = db.Column(db.String(64), nullable=False)
+	cookie = db.Column(db.String(255), unique=True, nullable=True)
+	OTPSecret = db.Column(db.String(255), unique=True, nullable=True)
+	codeEP = db.Column(db.String(255), unique=True, nullable=True)
+	picture = db.Column(db.String(1024), unique=True, nullable=True)
+	enable = db.Column(db.Integer, nullable=True, default=1)
+	firstCon = db.Column(db.Integer, default=0)
 
-    def set_password(self, password):
-        self.password = bcrypt.generate_password_hash(password).decode("utf-8")
+	def set_password(self, password):
+		self.password = bcrypt.generate_password_hash(password).decode("utf-8")
 
 
 class Job(db.Model):
-    __tablename__ = "Job"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    name = db.Column(db.String(255), nullable=False, unique=True)
+	__tablename__ = "Job"
+	id = db.Column(db.Integer, primary_key=True, unique=True)
+	name = db.Column(db.String(255), nullable=False, unique=True)
 
 
 class USBlog(db.Model):
-    __tablename__ = "USBlog"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    virus = db.Column(db.Integer, default=0)
-    date = db.Column(DateTime(timezone=True), server_default=func.now())
-    idUser = db.Column(db.Integer, db.ForeignKey("Users.id"))
-    idSlave = db.Column(db.Integer, db.ForeignKey("Endpoint.id"))
+	__tablename__ = "USBlog"
+	id = db.Column(db.Integer, primary_key=True, unique=True)
+	virus = db.Column(db.Integer, default=0)
+	date = db.Column(DateTime(timezone=True), server_default=func.now())
+	idUser = db.Column(db.Integer, db.ForeignKey("Users.id"))
+	idSlave = db.Column(db.Integer, db.ForeignKey("Endpoint.id"))
 
 
-class Endpoint(db.Model):
-    __tablename__ = "Endpoint"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    ip = db.Column(db.String(64), nullable=False, unique=True)
-    token = db.Column(db.String(255), nullable=False, unique=True)
-    hostname = db.Column(db.String(255), nullable=True)
-    state = db.Column(db.Integer, nullable=False)
+class Devices(db.Model):
+	__tablename__ = "Devices"
+	id = db.Column(db.Integer, primary_key=True, unique=True)
+	ip = db.Column(db.String(64), nullable=False, unique=True)
+	hostname = db.Column(db.String(255), nullable=True)
+	description = db.Column(db.String(1024), nullable=True)
+	token = db.Column(db.String(255), nullable=False, unique=True)
+	state = db.Column(db.Integer, nullable=False)
 
+class Metrics(db.Model):
+	__tablename__ = "Metrics"
+	id = db.Column(db.Integer, primary_key=True, unique=True)
+	name = db.Column(db.String(128),unique=False,nullable=False)
+	value = db.Column(db.Integer,unique=False,nullable=False)
+	idDevice = db.Column(db.Integer, db.ForeignKey("Devices.id"))
 
 # class Alerts(db.Model):
 #    __tablename__ = "Alerts"
@@ -61,7 +68,7 @@ class Endpoint(db.Model):
 
 
 class Setup(db.Model):
-    __tablename__ = "Setup"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    action = db.Column(db.String(255), nullable=False, unique=True)
-    state = db.Column(db.Boolean, default=False)
+	__tablename__ = "Setup"
+	id = db.Column(db.Integer, primary_key=True, unique=True)
+	action = db.Column(db.String(255), nullable=False, unique=True)
+	state = db.Column(db.Boolean, default=False)
