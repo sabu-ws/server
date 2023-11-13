aa6fb7824027105d98a70854f9f373c63064e9b2
LooXzy <looxzy@protonmail.com>
2023-11-13 12:17:56 +0100

    update remote ssh system

--- app/blueprints/panel/server/server_bp.py
+++ app/blueprints/panel/server/server_bp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from flask_socketio import SocketIO, rooms, disconnect
 
 from app import logout_user, socketio
