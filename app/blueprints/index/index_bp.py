1afbdbc18ae9f40ff5d6832ab15b985dd8393a51
tranosgames <pubtranos@gmail.com>
2023-11-14 03:13:55 -0500

    modify redirection

--- app/blueprints/index/index_bp.py
+++ app/blueprints/index/index_bp.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, redirect
+from flask import Blueprint, redirect, url_for
+
+from app import current_user
 
 from config import *
 
@@ -8,4 +10,10 @@ index_bp = Blueprint("index", __name__, template_folder="templates")
 
 @index_bp.route("/", methods=["GET"])
 def index():
-    return redirect("/login")
+	if current_user.is_authenticated:
+		if current_user.role == "Admin":
+			return redirect(url_for("panel.users.index"))
+		elif current_user.role == "User":
+			return redirect(url_for("panel.users.index"))
+	else:
+		return redirect("/login")
