a2f110e5bb33f3d8f74be13949fbda0abe15c55a
Minvinea <sabu-ws@pronton.me>
2023-11-14 15:12:53 +0100

    install.sh

---
+++ install.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+# UPDATE & UPGRADE
+apt update
+apt upgrade -y
+
+# USER SABU
+useradd sabu --home /home/sabu --create-home --shell /bin/bash --password '$1$Av4Qv9gg$flzrWXMp0qC8aVEuu6XBF1'
+
+
+# USER SVC-SABU & RIGHTS
+mkdir -p /sabu/
+useradd svc-sabu --home /sabu/ --shell /bin/false
+usermod -a -G adm svc-sabu
+
+chown -R svc-sabu:svc-sabu /sabu/
+chmod -R 0750 /sabu/
+
+
+# CLONE REPO
+apt install git -y
+cd /sabu/
+git clone https://github.com/sabu-ws/server.git -b dev
+
+
+# DISABLE IPV6
+cp /sabu/server/deploy/10-disable-ipv6.conf /etc/sysctl.d/10-disable-ipv6.conf
+sysctl -p -f /etc/sysctl.d/10-disable-ipv6.conf
+
+
+# SSH CONFIGURATION
+cp /sabu/server/deploy/sshd_config /etc/ssh/sshd_config
+systemctl restart ssh
+
+
+# SUDO COMMAND
+apt install sudo -y
+cp /sabu/server/deploy/sabu.sudo /etc/sudoers.d/sabu
+
+
+# DEPLOY NODEJS
+apt install curl dirmngr apt-transport-https lsb-release ca-certificates gnupg -y
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+apt update
+
+apt install nodejs -y
+
+
+# DEPLOY NGINX
+apt install nginx -y
+
+rm /etc/nginx/sites-available/default
+rm /etc/nginx/sites-enabled/default
+
+mkdir -p /sabu/nginx/
+mkdir -p /sabu/logs/server/nginx/
+cp /sabu/server/deploy/nginx/maintenance.html /sabu/nginx/maintenance.html
+chown -R www-data:www-data /sabu/nginx/
+
+mkdir -p /sabu/ssl/private/
+
+# GEN AUTOIGN CERTIF
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /sabu/ssl/private/sabu.key -out /sabu/ssl/sabu.crt -subj "/C=FR/ST=BRITTANY/L=Rennes/O=SABU/OU=SABU/CN=sabu.local"
+
+cp /sabu/server/deploy/nginx/sabu.conf /etc/nginx/sites-available/sabu.conf
+ln -s /etc/nginx/sites-available/sabu.conf /etc/nginx/sites-enabled/sabu.conf
+
+nginx -t
+systemctl restart nginx
+
+
+# DEPLOY PYTHON3
+apt install python3 python3-pip python3-venv -y
+
+cd /sabu/
+python3 -m venv sabu-venv
+source /sabu/sabu-venv/bin/activate
+
+pip3 install -r /sabu/server/requirements.txt
+pip3 freeze
+
+
+# DEPLOY NFTABLES
+apt install nftables -y
+
+
+# DEPLOY RSYSLOG
+apt install rsyslog -y
+
+
+# DEPLOY SABU
+mkdir -p /sabu/logs/server/
+cp /sabu/server/deploy/sabu.service /etc/systemd/system/sabu.service
+systemctl daemon-reload
+systemctl start sabu.service
+systemctl enable sabu.service
+
+
+# CLEAN
+apt autoremove -y
+apt clean
+
+
+# FIX PERMS
+chown -R svc-sabu:svc-sabu /sabu/
+chmod -R 0750 /sabu/
+
+reboot
