9feaf23d2507b9068dc54aaf056e907e54edf7f0
tranosgames <pubtranos@gmail.com>
2023-11-16 14:50:20 +0100

    script cpu

--- core/scripts/get_ram_space.sh
+++ core/scripts/get_ram_space.sh
@@ -3,6 +3,4 @@
 
 used=$(vmstat | tail -1 | awk '{print $4}')
 total=$(vmstat -s | grep "total memory" | awk '{print $1}')
-echo $used $total
-#DF=$(df -h / --output=used,avail | tail -n 1 | awk '{print $1,$2}' | tr -d 'G')
-#echo $DF  # return used, availaible
+echo $used $total # used total
