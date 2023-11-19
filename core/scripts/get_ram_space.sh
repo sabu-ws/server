fc6f6d492cefa89dd77c6eb65bd4e1c661992f50
tranosgames <pubtranos@gmail.com>
2023-11-19 17:26:31 +0100

    new script for ram space

--- core/scripts/get_ram_space.sh
+++ core/scripts/get_ram_space.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 
 #used=$(vmstat | tail -1 | awk '{print $4}')
-used=$(vmstat -s | grep "used memory" | awk '{print $1}')
-total=$(vmstat -s | grep "total memory" | awk '{print $1}')
+used=$(vmstat -s | head -n 2 | tail -n 1 | awk '{print $1}')
+total=$(vmstat -s | head -n 1 | awk '{print $1}')
 echo $used $total # used total
