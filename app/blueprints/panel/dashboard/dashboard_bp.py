52a4b44c12aca6fc66e121f7cee2189a82bc410d
LooXzy <looxzy@protonmail.com>
2023-12-06 14:21:27 +0100

    update dashboard page

--- app/blueprints/panel/dashboard/dashboard_bp.py
+++ app/blueprints/panel/dashboard/dashboard_bp.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-
+from app.models import Devices
 from config import *
 
 dashboard_bp = Blueprint("dashboard", __name__)
@@ -7,4 +7,6 @@ dashboard_bp = Blueprint("dashboard", __name__)
 
 @dashboard_bp.route("/")
 def index():
-    return render_template("ap_dashboard.html")
+    devices=Devices.query.filter(Devices.token!="server").all()
+    return render_template("ap_dashboard.html", list_devices=devices)
+    
