95abfb077e77c1a6e2c32ca9f10291301fa000f7
tranosgames <pubtranos@gmail.com>
2023-12-13 11:54:07 -0600

    fix js

--- app/static/js/scripts/browser_srv.js
+++ app/static/js/scripts/browser_srv.js
@@ -6,3 +6,21 @@ $("tr").click(function(){
 	}
 });
 
+$(function(){
+	if(window.location.pathname != "/panel/browser/path/"){
+		$("#returnButton").removeAttr('hidden');
+	}
+});
+
+$("#returnButton").click(function(){
+	var url = window.parent.location.href;
+	if(url.substr(url.length - 1) == "/"){
+		url=url.slice(0,-1)
+	}
+	if(url.slice(url.lastIndexOf('/')) != '/panel/browser/path/'){
+		var to = url.lastIndexOf('/');
+		to = to == -1 ? url.length : to + 1;
+		url = url.substring(0, to -1 );
+		window.location=url;
+	}
+});
