ac505d9ec6e83d15406c9ce7f7389bc62e12e03f
tranosgames <pubtranos@gmail.com>
2024-01-11 18:51:22 +0100

    modify dashboard endpoint number

--- app/blueprints/panel/dashboard/dashboard_bp.py
+++ app/blueprints/panel/dashboard/dashboard_bp.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from app import logger as log
 from app.models import Devices
 from app.utils.system import NET_get_ip_server
 from app.utils.system import SYS_get_hostname, SYS_get_uptime
@@ -10,6 +11,8 @@ dashboard_bp = Blueprint("dashboard", __name__)
 @dashboard_bp.route("/")
 def index():
     devices = Devices.query.filter(Devices.token != "server").all()
+    number_endpoint_on = Devices.query.filter(Devices.token != "server").filter(Devices.state==1).count()
+    number_endpoint_tot = len(devices)
     server_ip = NET_get_ip_server()
     hostname = SYS_get_hostname()
     uptime = SYS_get_uptime()
@@ -17,6 +20,8 @@ def index():
     licence = SABU_LICENCE
     return render_template(
         "ap_dashboard.html",
+        number_endpoint_on=number_endpoint_on,
+        number_endpoint_tot=number_endpoint_tot,
         list_devices=devices,
         hostname=hostname,
         uptime=uptime,
