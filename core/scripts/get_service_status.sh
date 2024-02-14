e6d83c27536267e31d1985bc3d376cc012e47cb5
Mathis BINET <29579830+Minvinea@users.noreply.github.com>
2024-02-15 00:04:48 +0100

    Add script get service status

---
+++ core/scripts/get_service_status.sh
@@ -0,0 +1,17 @@
+# SABU-SERVER
+#!/bin/bash
+
+SERVICE_NAME=$1
+SERVICE_STATUS=$(systemctl is-active $SERVICE_NAME | grep -E "[a-z]+")
+
+if [[ $SERVICE_STATUS == "active" ]];
+then
+    SERVICE_TIMESTAMP=$(systemctl show -p ActiveEnterTimestamp $SERVICE_NAME --value)
+    SERVICE_RUNTIME=$(( ( $(date +%s) - $(date -d "$SERVICE_TIMESTAMP" +%s) ) / 60 ))
+
+    echo "SERVICE_NAME: $SERVICE_NAME; STATUS: $SERVICE_STATUS; RUNTIME: $SERVICE_RUNTIME;"
+
+elif [[ $SERVICE_STATUS == "inactive" ]];
+then
+    echo "SERVICE_NAME: $SERVICE_NAME; STATUS: $SERVICE_STATUS;"
+fi
