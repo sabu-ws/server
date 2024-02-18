67f777072b48e52650f752d3612024a16ef11708
Minvinea <sabu-ws@pronton.me>
2024-02-18 19:42:08 +0100

    Fix display

--- core/scripts/get_service_status.sh
+++ core/scripts/get_service_status.sh
@@ -9,7 +9,7 @@ then
     SERVICE_TIMESTAMP=$(systemctl show -p ActiveEnterTimestamp $SERVICE_NAME --value)
     SERVICE_RUNTIME=$(( ( $(date +%s) - $(date -d "$SERVICE_TIMESTAMP" +%s) ) / 60 ))
 
-    echo "SERVICE_NAME: $SERVICE_NAME; STATUS: $SERVICE_STATUS; RUNTIME: $SERVICE_RUNTIME;"
+    echo "SERVICE_NAME:$SERVICE_NAME\nSTATUS:$SERVICE_STATUS\nRUNTIME:$SERVICE_RUNTIME"
 
 elif [[ $SERVICE_STATUS == "inactive" ]];
 then
