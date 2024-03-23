65761b9b410b624062eb903782d46003038e1650
Mathis BINET <29579830+Minvinea@users.noreply.github.com>
2024-03-23 23:52:21 +0100

    add script v1 scan clamav

---
+++ core/scripts/scan_clamav.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while getopts ":s:q:l:" opt
+do
+    case $opt in
+        s)  SCAN_PATH=$OPTARG
+            ;;
+        q)
+            QUARANTINE_PATH=$OPTARG
+            ;;
+        l)
+            LOG_PATH=$OPTARG
+            ;;
+    esac
+done
+
+if [ -z "$SCAN_PATH" ] || [ -z "$QUARANTINE_PATH" ] || [ -z "$LOG_PATH" ]
+then    
+    echo "$0 -s <str:SCAN_PATH> -q <str:QUARANTINE_PATH> -l <str:LOG_PATH>"
+    exit 1
+fi
+
+# VARS
+TIMESTAMP=$(date +%s)
+LOG_NAME="${LOG_PATH}/clamav_${TIMESTAMP}.log"
+
+# SCAN
+clamscan --recursive=yes --log=$LOG_NAME --move=$QUARANTINE_PATH $SCAN_PATH
+SCAN_STATUS=$?
+
+# RESULT
+echo "${LOG_NAME};${SCAN_STATUS}"
