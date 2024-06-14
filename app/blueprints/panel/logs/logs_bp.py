5f45015a7eb7ef0717875047589ff43f641199db
tranosgames <pubtranos@gmail.com>
2024-06-14 11:12:58 +0200

    Add sabu logs server

--- app/blueprints/panel/logs/logs_bp.py
+++ app/blueprints/panel/logs/logs_bp.py
@@ -1,9 +1,23 @@
 from flask import Blueprint, render_template
-
+from app import socketio, logger as log
 from config import *
 
 logs_bp = Blueprint("logs", __name__)
 
+@socketio.on("startLogsSabu", namespace="/logServer")
+def startLogsServer():
+    temp_send_file = open("/sabu/logs/server/sabu.log").read()
+    modify_syslog = os.stat("/sabu/logs/server/sabu.log")[9]  # modify time
+    temp_modify_syslog = ""
+    while True:
+        socketio.sleep(1)
+        if temp_modify_syslog != modify_syslog:
+            temp_modify_syslog = modify_syslog
+            file_syslog = open("/sabu/logs/server/sabu.log").read()
+            restrict_len = file_syslog[len(file_syslog)-100000:]
+            socketio.emit("receiveLogsSabu", restrict_len, namespace="/logServer")
+        else:
+            modify_syslog = os.stat("/sabu/logs/server/sabu.log")[9]
 
 @logs_bp.route("/")
 def index():
--- app/static/js/scripts/logs.js
+++ app/static/js/scripts/logs.js
@@ -1,9 +1,9 @@
 // Socketio
 $(document).ready(function(e){
-	if(document.location.pathname=="/panel/logs"){
+	if(document.location.pathname=="/panel/logs/"){
 		var socket = io.connect("/logServer");
-		socket.emit("startLogsServer");
-		socket.on("receiveLogs",function(data){
+		socket.emit("startLogsSabu");
+		socket.on("receiveLogsSabu",function(data){
 			$("#setLogs").html(data.replace(/\n/g, "<br />"));
 			$("#masterSetLogs").animate({ scrollTop: $("#masterSetLogs")[0].scrollHeight }, 1000);
 		})
